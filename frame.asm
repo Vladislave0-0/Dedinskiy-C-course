33954dcae0d700c074f833deb0e2127e3f2ace1a
Vlad Lazar <lazar_2004@list.ru>
2024-02-28 15:34:59 +0300

    Fixed the printout of the user message in the middle of the screen. Added comments and documentation

--- frame.asm
+++ frame.asm
@@ -4,11 +4,12 @@
 org 100h				; location in the memory of the first command
 LOCALS @@               ; directive for local labels
 
-;==========================================================================================================
+;========================================================================================================================
 
-SCREEN_WIDTH equ 160d
-VMEM_SEGMENT equ 0b800h
-H_OFFSET     equ SCREEN_WIDTH*5
+SCREEN_WIDTH  equ 160d
+SCREEN_HEIGHT equ 25d
+VMEM_SEGMENT  equ 0b800h
+H_OFFSET      equ SCREEN_WIDTH*5
 
 
 EXIT	macro
@@ -18,34 +19,39 @@ EXIT	macro
 		nop
 		endm
 
-;=========================================================================================================
+;=======================================================================================================================
 ; string example for running the code: frame.com   a       b        c           d         e         &
 ;                                      ^           ^       ^        ^           ^         ^         ^
 ;                                      prog_name   width   height   attribute   pattern   message   endL
-;=========================================================================================================
+;=======================================================================================================================
 
 Start:
         mov bx, VMEM_SEGMENT
         mov es, bx
         xor bx, bx
 
-
         call ParseString
         call PrintFrame
 
+        xor ax, ax              ; waits for the key to be pressed
+        mov ah, 08h
+        int 21h
+
         EXIT
 
-;=================================================================
+
+
+;===============================================================================
 ; Parses the entered string.
-;-----------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ; Enter:    None
 ; Exit:     None
 ; Expects:  ES = 0b800h
 ; Destroys: None
-;=================================================================
+;===============================================================================
 ParseString     proc
 
-                push 0082h             ; command line start adress
+                push 0082h             ; command line start adress 
                 pop si
 
 
@@ -59,15 +65,13 @@ ParseString     proc
 
                 call ParseHex          ; parsing attribute
                 push ax
-                inc si
+                inc si                 ; skip 'h' in end of hex num
                 call SkipSpaces
 
                 call ParsePattern      ; parsing pattern
-                push ax
 
 
                 xor dx, dx             ; save pattern type in DH
-                pop ax
                 mov dh, al
 
                 pop ax                 ; save attribute in DL
@@ -84,19 +88,19 @@ ParseString     proc
 
 
 
-;=================================================================
+;===============================================================================
 ; Parses the type of pattern (programm or user-defined).
-;-----------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ; Enter:    SI - pointer to pattern part of cmd line adress
 ; Exit:     AX - type of pattern
 ; Expects:  None
 ; Destroys: None
-;=================================================================
+;===============================================================================
 ParsePattern    proc
 
                 lodsb
-                cmp al, '*'
-                jz @@UserPattern
+                cmp al, '*'         ; if(AL == '*') {goto UserPattern}
+                jz @@UserPattern    ; else {use programm pattern}
                 sub al, '1'
                 jmp @@PatternExit
 
@@ -107,28 +111,28 @@ ParsePattern    proc
 
 
 
-;=================================================================
+;===============================================================================
 ; Parses the entered decimal number from ASCII.
-;-----------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ; Enter:    SI - pointer to part with dec of cmd line adress
 ; Exit:     AX - number
 ; Expects:  None
 ; Destroys: AX, BX, CX, DX
-;=================================================================
+;===============================================================================
 ParseDec        proc
 
-                push si             ; save SI
-                push di             ; save DI
+                push si di          ; save registers
                 mov di, si          ; DI = SI
                 mov dx, 1d          ; DX = 1
                 xor ax, ax          ; AX = 0
                 xor bx, bx          ; BX = 0
                 xor cx, cx          ; CX = 0
 
-@@ReadLen:      lodsb
+@@ReadLen:      lodsb               ; todo call strlen
                 inc cl              ; CL++ 
                 cmp al, ' '         ; if AL == ' ' then ZF = 1 
                 jnz @@ReadLen
+
                 dec cl              ; CL--  
                 push cx             ; save CX
 
@@ -142,20 +146,18 @@ ParseDec        proc
                 lodsb
                 cmp al, ' '         ; if AL == ' ' then ZF = 1 
                 jz @@EndNum
-                sub al, 30h         ; from ASCII to number
+                sub al, '0'         ; from ASCII to number
                 push dx             ; save DX
                 mul dx              ; AX *= 10^(digit-1)
                 pop dx              ; saved DX
                 add bx, ax          ; BX += AX
                 mov ax, dx          ; DX *= 10
-                mul cx
+                mul cx              ; AX *= CX
                 mov dx, ax          ; DX = AX
                 dec di              ; DI--
                 jmp @@NextDigit
 
-@@EndNum:       pop cx              ; saved CX
-                pop di              ; saved DI
-                pop si              ; saved SI
+@@EndNum:       pop cx di si        ; saved registers
                 add si, cx          ; SI += len of number
                 mov ax, bx          ; AX = BX
 
@@ -164,18 +166,17 @@ ParseDec        proc
 
 
 
-;=================================================================
+;===============================================================================
 ; Parses the entered hexagonal number.
-;-----------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ; Enter:    SI - pointer to part with hex of cmd line adress
 ; Exit:     AX - number
 ; Expects:  None
 ; Destroys: AX, BX, CX, DX
-;=================================================================
+;===============================================================================
 ParseHex        proc
 
-                push si             ; save SI
-                push di             ; save DI
+                push si di          ; saved registers
                 mov di, si          ; DI = SI
                 mov dx, 1d          ; DX = 1
                 xor ax, ax          ; AX = 0
@@ -202,25 +203,23 @@ ParseHex        proc
 
                 cmp al, ':'         ; check if it is number or letter
                 jb @@IsNumber
-                sub al, 57h         ; from ASCII to hex
+                sub al, 'a' - 0Ah   ; from ASCII to hex 
                 jmp @@IsLetter
 
 
-@@IsNumber:     sub al, 30h         ; from ASCII to hex
+@@IsNumber:     sub al, '0'         ; from ASCII to hex
 @@IsLetter:     push dx             ; save DX
                 mul dx              ; AX *= 16^(digit-1)
                 pop dx              ; saved DX
                 add bx, ax          ; BX += AX
                 mov ax, dx          ; DX *= 16
-                mul cx
+                mul cx              ; AX *= CX
                 mov dx, ax          ; DX = AX
                 dec di              ; DI--
                 jmp @@NextDigit
 
 
-@@EndNum:       pop cx              ; saved CX
-                pop di              ; saved DI
-                pop si              ; saved SI
+@@EndNum:       pop cx di si        ; saved registers
                 add si, cx          ; SI += len of number
                 mov ax, bx          ; AX = BX
 
@@ -229,17 +228,17 @@ ParseHex        proc
 
 
 
-;=================================================================
+;===============================================================================
 ; Skips all spaces.
-;-----------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ; Enter:    SI - pointer to part of cmd line adress
 ; Exit:     new SI with skipped spaces
 ; Expects:  None
 ; Destroys: None
-;=================================================================
+;===============================================================================
 SkipSpaces      proc
 
-@@Next:         lodsb                
+@@Next:         lodsb               ; AL = DS:[SI++]
                 cmp al, ' '         ; if AL == ' ' then ZF = 1
                 jz @@Next
 
@@ -250,44 +249,42 @@ SkipSpaces      proc
 
 
 
-;=================================================================
+;===============================================================================
 ; Prints frame.
-;-----------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ; Enter:    BL - width, BH - height
 ;           DL - attribute, DH - pattern
 ; Exit:     None
 ; Expects:  ES = 0b800h
 ; Destroys: AX, DI
-;=================================================================
+;===============================================================================
 PrintFrame  proc
 
-            push bx                                         ; save BX
-            push dx                                         ; save DX
-            mov ah, dl                                      ; frame attribute
-            cld                                             ; DF = 0 => DI++
-            push si                                         ; save SI
+            push bx dx si       ; save registers
+            mov ah, dl          ; AH = attribute
+            cld                 ; DF = 0 => DI++
 
 
             ; PATTERN SELECTION
-            cmp dh, 4                                       ; if pattern is wrong (type > 1 or type < 0)
-            ja @@ErrPtrn
+            cmp dh, 4           ; if(pattern is wrong (type > 1 or type < 0))
+            ja @@ErrPtrn        ; {goto ErrorPattern}
             cmp dh, 0
             jb @@ErrPtrn 
-            cmp dh, 4                                       ; if the pattern is custom
-            jz @@UserPtrn                                   ; if program pattern
+            cmp dh, 4           ; if the pattern is custom
+            jz @@UserPtrn       ; if program pattern
             jmp @@ProgPtrn
 
 @@ProgPtrn: call ProgPattern
-@@UserPtrn: xor dx, dx                                      ; DX = 0
-            mov dl, bl                                      ; for (CX - counter)
+@@UserPtrn: xor dx, dx          ; DX = 0
+            mov dl, bl          ; for (CX - counter)
 
 
-            mov di, SCREEN_WIDTH/2 + H_OFFSET - 2           ; start position of printout
-            push ax
-            xor ax, ax
-            mov al, bl
-            sub di, ax
-            pop ax
+            mov di, SCREEN_WIDTH/2 + H_OFFSET - 2d  ; start position of printout
+            push ax                                 ; save AX
+            xor ax, ax                              ; AX = 0
+            mov al, bl                              ; AL = Bl
+            sub di, ax                              ; DI -= Ax
+            pop ax                                  ; saved Ax
 
 
             ; FRAME PRINTOUT
@@ -312,9 +309,9 @@ PrintFrame  proc
 
 
             ; USER MESSAGE PRINTOUT
-            cmp dh, 4
-            jz @@PrntMsg1
-            inc si                      ; skip pattern
+            cmp dh, 4                   ; if(DH == 4) {skip 9 symbols}
+            jz @@PrntMsg1               ; else {skip 1 symbol}
+            inc si                      ; SI++
             jmp @@PrntMsg2
 
 @@PrntMsg1: add si, 9                   ; if pattern from user, then skip all pattern
@@ -326,74 +323,69 @@ PrintFrame  proc
 
 
 
-;=================================================================
+;===============================================================================
 ; Prints user's message in the middle of the frame.
-;-----------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ; Enter:    BL - width, BH - height
 ;           DL - attribute, DH - pattern
 ; Exit:     None
 ; Expects:  SI - pointer to user message
 ; Destroys: AX, BX
-;=================================================================            
+;===============================================================================            
 PrintMsg    proc
 
-            push cx         ; save CX
-            push di         ; save DI
-            push dx         ; save Dx
-            push si         ; save SI
-            xor cx, cx      ; CX = 0
-            xor di, di      ; Dx = 0
+            push cx di dx si    ; save registers   
+            xor cx, cx          ; CX = 0
+            xor di, di          ; DX = 0
+
 
             ; COUNTING LENGTH OF USER MESSAGE
-@@CntLen:   lodsb
-            cmp al, '&'
-            jz @@ExitLen
+@@CntLen:   lodsb               ; AL = DS:[SI++]
+            cmp al, '&'         ; if(AL == '&') {goto ExitLen}
+            jz @@ExitLen        ; else {goto CountLen}
             inc cx
             jmp @@CntLen
-@@ExitLen:  dec cx
-            pop si
+@@ExitLen:  pop si              ; saved SI
 
             ; CALCULATION OF MESSAGE OFFSET
-            mov ah, dl
-            shr cx, 1
-            sub di, cx
-            add di, H_OFFSET + 76d
-            xor cx, cx
-            mov cl, bh
-            dec cx
-            shr cx, 1
-            mov ax, cx
-            mov cx, SCREEN_WIDTH
-            mul cx
-            add di, ax
-            and di, 65534d
+            and cx, 0fffeh                      ; CX is even to access an even cell of video memory
+            sub di, cx                          ; DI -= CX
+            add di, H_OFFSET + SCREEN_WIDTH/2   ; DI += offset from the top
+            xor cx, cx                          ; CX = 0
+            and bx, 0feffh                      ; lower bit of BH = 0
+            mov cl, bh                          ; CL = BH
+            mov ax, SCREEN_WIDTH/2              ; AX = 80d
+            mul cx                              ; AX *= CX
+            add di, ax                          ; DI += AX
+            and di, 0fffeh                      ; make DI even
 
-            pop dx
-            mov ah, dl
+            pop dx                              ; save DX
+            mov ah, dl                          ; AH = DL
+
 
             ; PRINTOUT OF MESSAGE
-@@Next:     lodsb
-            cmp al, '&'
-            jz @@Exit
-            stosw
+@@Next:     lodsb           ; AL = DS:[SI++]
+            cmp al, '&'     ; if(AL == '&') {goto Exit}
+            jz @@Exit       ; else {print symbol; loop}
+            stosw           ; ES:[DI] = AX, DI += 2
             jmp @@Next
 
 
-@@Exit:  	pop di      ; saved DI
-            pop cx      ; saved CX
+@@Exit:  	pop di cx       ; save DI, CX
+
             ret
 			endp	
 
 
         
-;=================================================================
+;===============================================================================
 ; Sets the desired offset to print the desired pattern.
-;-----------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ; Enter:    DH - type number of the programm pattern
 ; Exit:     SI - offset of control string
 ; Expects:  DH - type of pattern
 ; Destroys: None
-;=================================================================
+;===============================================================================
 ProgPattern proc
 
             push ax                         ; save AX
@@ -414,36 +406,34 @@ ProgPattern proc
 
 
 
-;=================================================================
+;===============================================================================
 ; Draws one line of the frame.
-;-----------------------------------------------------------------
+;-------------------------------------------------------------------------------
 ; Enter:    DX - counter for middle elements
 ;           DI - vmem position on screen
 ;           SI - source of frame element
 ; Exit:     None
 ; Expects:  ES = 0b800h
 ; Destroys: AX
-;=================================================================
+;===============================================================================
 PrintLine   proc
 
-		    push di                 ; save DI
-		    push cx                 ; save CX
-            mov cx, dx
+		    push di cx              ; save registers
+            mov cx, dx              ; CX = DX
 
             ; LEFT CHARACTER
             lodsb                   ; AL = DS:[SI++]
             stosw                   ; ES:[DI] = AX, DI += 2
 
             ; MIDDLE CHARACTERS
-            lodsb
-            rep stosw
+            lodsb                   ; AL = DS:[SI++]
+            rep stosw               ; while(CX--) {ES:[DI] = AX; DI += 2;}
 
             ; RIGHT CHARACTER
-            lodsb
-            stosw
+            lodsb                   ; AL = DS:[SI++]
+            stosw                   ; ES:[DI] = AX, DI += 2
 
-            pop cx                  ; saved CX
-            pop di                  ; saved DI
+            pop cx di               ; saved registers
             add di, SCREEN_WIDTH    ; DI += 160 for next line
 
 		    ret
@@ -451,6 +441,7 @@ PrintLine   proc
 
 
 
+;===============================================================================
 control_string  db 218d, 196d, 191d, 179d, ' ', 179d, 192d, 196d, 217d
                 db 201d, 205d, 187d, 186d, ' ', 186d, 200d, 205d, 188d
                 db  '+', 196d,  '+', 179d, ' ', 179d,  '+', 196d,  '+'
